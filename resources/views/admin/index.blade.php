6db5766d69611292217b21fe6b36895ba6c56eda
arturo escritorio <arturo221355@gmail.com>
2020-04-03 18:23:12 -0600

    aqui empiezo a experimentar las migraciones

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,19 +3,6 @@
 @section('content')
 
 
-<table>
 
-@foreach ($iccs as $icc)
-
-<tr>
-
-<td>{{$icc->subproduct->name}}</td>
-<td>{{$icc->subproduct->product->name}}</td>
-
-</tr>
-
-
-@endforeach
-</table>
 
 @endsection
