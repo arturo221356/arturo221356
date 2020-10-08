91ebfb5b8a752bd2d96bf9e4c17cf1d11251b0a4
arturo escritorio <arturo221355@gmail.com>
2020-10-08 11:11:09 -0500

    qweqwe

--- config/database.php
+++ config/database.php
@@ -4,12 +4,7 @@ use Illuminate\Support\Str;
 
  $DATABASE_URL=parse_url('postgres://sujiakylqnrkqk:cededc65c295f827420830614538f47314e58169af5e493572fe61c13f70460e@ec2-34-192-122-0.compute-1.amazonaws.com:5432/dav9mcapv1jpbq');
 
-// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-// $host = $url["host"];
-// $username = $url["user"];
-// $password = $url["pass"];
-// $database = substr($url["path"], 1);
 
 return [
 
@@ -101,7 +96,7 @@ return [
             'port' => $DATABASE_URL["port"],
            'database' => ltrim($DATABASE_URL["host"],"/"),
             'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["password"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
