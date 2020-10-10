a02468cb1a62ee90c739093e6cf966bc3ed8d39b
arturo escritorio <arturo221355@gmail.com>
2020-10-10 09:04:54 -0500

    sadasdas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
 
     <script type="text/javascript">
       window.Laravel = {
-          // csrfToken: "{{ csrf_token() }}",
+           csrfToken: "{{ csrf_token() }}",
           jsPermissions: {!! auth()->check()?auth()->user()->jsPermissions():null !!}
       }
     </script> 
@@ -38,7 +38,7 @@
                 @auth
                 <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="/venta">Ventas</a>
