293e54ebea8678a9740a8578ed9bd39eab2125a0
arturo escritorio <arturo221355@gmail.com>
2020-11-29 14:30:39 -0600

    asdasd

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'notificaciones@ventachip.com'),
-        'name' => env('MAIL_FROM_NAME', 'Promoviles'),
+        'address' => 'notificaciones@ventachip.com',
+        'name' => 'Promoviles',
     ],
 
     /*
