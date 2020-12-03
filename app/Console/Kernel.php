f10a173f4bbf3a25749f9a858c5703aea01a0dee
arturo escritorio <arturo221355@gmail.com>
2020-12-03 17:51:15 -0600

    asdads

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 
-        $schedule->command('exportados:daily')->dailyAt('04:00');
+        // $schedule->command('exportados:daily')->dailyAt('04:00');
     }
 
     /**
