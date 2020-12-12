4a28d5a41b8d71a58dbd809ccf0f399c4b8e1d99
arturo escritorio <arturo221355@gmail.com>
2020-12-12 12:12:51 -0600

    se cambio el formato de la hora

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 
-        $schedule->command('exportados:daily')->dailyAt('06:00');
+        $schedule->command('exportados:daily')->dailyAt('6:00');
     }
 
     /**
