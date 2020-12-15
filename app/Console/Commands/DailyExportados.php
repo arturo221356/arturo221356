dc0464096abb6db1b78395afff00da9aa4b47626
Arturo Alvarez <arturo221355@gmail.com>
2020-12-14 22:42:32 -0600

    cambie de info a linea

--- app/Console/Commands/DailyExportados.php
+++ app/Console/Commands/DailyExportados.php
@@ -77,7 +77,7 @@ class DailyExportados extends Command
 
                 $inventarioName = $linea->icc->inventario->inventarioable->name;
 
-                $this->info($linea->dn." Exportada a ".$response->result[0]->value."-------".$inventarioName);
+                $this->line($linea->dn." Exportada a ".$response->result[0]->value."-------".$inventarioName);
             }
         }
 
@@ -100,7 +100,7 @@ class DailyExportados extends Command
 
                 $inventarioName = $linea->icc->inventario->inventarioable->name;
 
-                $this->info($linea->dn." Exportada a ".$response->result[0]->value."-------".$inventarioName);
+                $this->line($linea->dn." Exportada a ".$response->result[0]->value."-------".$inventarioName);
             }
             
             
