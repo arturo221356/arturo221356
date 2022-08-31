955ef93ef04586de5814f29e91fbe2fce0e65e31
Arturo Alvarez <arturo221355@gmail.com>
2022-08-31 14:01:59 -0500

    se recorta el icc a 19 digitos

--- app/Imports/TelcelPortaImport.php
+++ app/Imports/TelcelPortaImport.php
@@ -36,7 +36,7 @@ class TelcelPortaImport implements ToCollection
         foreach ($rows as $key => $row) {
             $numero = $row[1];
             $nip = $row[2];
-            $icc = $row[0];
+            $icc = substr($row[0],0,19);
             $promo = '547';
             $loggedUser = Auth::user();
             $telcelUser = TelcelUser::where('distribution_id', $loggedUser->distribution_id)->first();
