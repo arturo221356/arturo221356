92c36028dc0027a4311cec14cd55eef6b19af90e
Arturo Alvarez <arturo221355@gmail.com>
2023-12-07 10:25:11 -0600

    adasd

--- app/TelcelUser.php
+++ app/TelcelUser.php
@@ -27,7 +27,7 @@ class TelcelUser extends Model
 
             $consulta = Http::contentType("application/json")->bodyFormat('json')->post($urlapi, [
                 'EndPoint' => 1,
-                "Entrada" => "{\"idDispositivo\":\"$iddispositivo\",\"Password\":\"$pass\",\"Region\":\"5\",\"Usuario\":\"$user\",\"versionApp\":\"4.7 231107S\"}",
+                "Entrada" => "{\"idDispositivo\":\"$iddispositivo\",\"Password\":\"$pass\",\"Region\":\"5\",\"Usuario\":\"$user\",\"versionApp\":\"4.7 231205S\"}",
                 "Metodo" => "1",
                 "Pantalla" => "0",
                 "Usuario" => "",
